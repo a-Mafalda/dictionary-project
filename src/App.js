287633c62591f6d33f8939a35697c8e8c3707e50
Mafalda <84099790+a-Mafalda@users.noreply.github.com>
2021-08-01 00:10:26 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ export default function App() {
       <Dictionary />
     </main>
     </div>
+    <footer className="App-footer">
+          <small> 
+             <a href="https://github.com/a-Mafalda/dictionary-project" target="_blank rel="noreferrer>
+               Open-sourced code
+             </a>
+             , by Mafalda Fonseca da Costa
+             </small>
+        </footer>
      </div>
       </div>
   );
