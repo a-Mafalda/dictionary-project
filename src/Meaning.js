f86107916be9de8c63af53fd753432266a534ac8
Mafalda <84099790+a-Mafalda@users.noreply.github.com>
2021-07-31 23:56:56 +0100

    rendered the word meaning

---
+++ src/Meaning.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Meaning(props) {
+    console.log(props.Meaning);
+;    return ( <div className="meaning">
+<h3>{props.meaning.partOfSpeech}</h3>
+{props.meaning.definitions.map(function(definition, index)
+{ 
+    return (
+        <div key={index}>
+            <p>{definition.definitions}
+            <br/>
+            <em>{definition.example}</em>
+            </p>
+            </div>
+    
+
+);
+
+})}
+</div>
+);
+}
